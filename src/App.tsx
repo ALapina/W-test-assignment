f0142844baeb91a05878e35f5f171183be5521eb
ALapina <larisa_42@mail.ru>
2021-04-19 22:15:15 +0300

    react router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import "./App.scss";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+//components
+import Header from "./components/Header";
+import UsersList from "./components/UsersList";
+import UserDetails from "./components/UserDetails";
+import CreateNewUser from "./components/CreateNewUser";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi there</h1>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/user">
+          <UserDetails />
+        </Route>
+        <Route path="/create-new-user">
+          <CreateNewUser />
+        </Route>
+        <Route path="/">
+          <UsersList />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>fetch them all</h1>
+    </header>
+  );
+};
+
+export default Header;
