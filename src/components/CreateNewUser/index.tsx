f89822ef900b7b9f7238cdc36227dc2a8bbc47bd
ALapina <larisa_42@mail.ru>
2021-04-26 19:24:07 +0300

    refactor and style create new user

--- src/components/CreateNewUser/index.tsx
+++ src/components/CreateNewUser/index.tsx
@@ -8,11 +8,17 @@
 
 // https://www.npmjs.com/package/react-date-picker
 
+//https://stackoverflow.com/questions/51222559/formik-yup-form-validation-not-working-as-expected-with-virtualizedselect
+//https://github.com/jquense/yup#mixednullableisnullable-boolean--true-schema
+
+// https://codesandbox.io/s/vxv6q4z5?file=/index.js:943-949
+
+// https://stackoverflow.com/questions/56312372/react-datepicker-with-a-formik-form
+
 import { useState } from "react";
 import PageHeader from "../PageHeader";
 import { postNewUser } from "../../utils";
 import { useTranslation } from "react-i18next";
-// import DatePicker from "react-datepicker";
 import DatePicker from "react-date-picker";
 import { Formik, FormikHelpers, FormikProps, Form, Field } from "formik";
 //Yup for form validation
@@ -47,127 +53,156 @@ const CreateNewUser = () => {
   const [savedData, setSavedData] = useState(null as Object | null);
   const { t } = useTranslation();
 
-  // TODO: Валидацию в use effect? что бы перевод происходил
   // Form Validation Schema
-  //https://stackoverflow.com/questions/51222559/formik-yup-form-validation-not-working-as-expected-with-virtualizedselect
-  //https://github.com/jquense/yup#mixednullableisnullable-boolean--true-schema
   const CreateNewUserSchema = Yup.object().shape({
     firstName: Yup.string()
-      .matches(/^[a-z\s]+$/i, "Only latin craracters pls")
-      .required(t("form-validation.required")),
+      .matches(/^[a-z\s]+$/i, t("form.form-validation.name"))
+      .required(t("form.form-validation.required")),
     lastName: Yup.string()
-      .matches(/^[a-z\s]+$/i, "Only latin craracters pls")
-      .required(t("form-validation.required")),
+      .matches(/^[a-z\s]+$/i, t("form.form-validation.name"))
+      .required(t("form.form-validation.required")),
     dateOfBirth: Yup.date()
-      .max(minDate, t("form-validation.over18years"))
-      .required(t("form-validation.required"))
+      .max(minDate, t("form.form-validation.over18years"))
+      .required(t("form.form-validation.required"))
       .nullable(),
   });
 
   return (
     <div className="wrapper">
       <PageHeader title={t("page-headers.part3")} showButton={false} />
-      <Formik
-        initialValues={initialValues}
-        validationSchema={CreateNewUserSchema}
-        onSubmit={(
-          values: FormValues,
-          { setSubmitting }: FormikHelpers<FormValues>
-        ) => {
-          postNewUser("https://reqres.in/api/users/", values)
-            .then((data) => {
-              setSavedData(data);
-              console.log(data);
-            })
-            .catch((error) => console.error(error));
-          setSubmitting(false);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          setFieldValue,
-          setFieldTouched,
-        }: FormikProps<FormValues>) => (
-          <Form>
-            <label>
-              First Name:
-              <Field
-                name="firstName"
-                placeholder="enter your first name"
-                required
-              />
-              {errors.firstName && touched.firstName && (
-                <div>{errors.firstName}</div>
-              )}
-            </label>
-            <br />
-            <label>
-              Last Name:
-              <Field
-                className={errors.lastName && touched.lastName ? "red" : null}
-                name="lastName"
-                placeholder="enter your last name"
-                required
-              />
-              {errors.lastName && touched.lastName ? (
-                <div>{errors.lastName}</div>
-              ) : null}
-            </label>
-            <br />
+      <div className="form-wrapper">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={CreateNewUserSchema}
+          onSubmit={(
+            values: FormValues,
+            { setSubmitting }: FormikHelpers<FormValues>
+          ) => {
+            postNewUser("https://reqres.in/api/users/", values)
+              .then((data) => {
+                setSavedData(data);
+                console.log(data);
+              })
+              .catch((error) => console.error(error));
+            setSubmitting(false);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            setFieldValue,
+            setFieldTouched,
+          }: FormikProps<FormValues>) => (
+            <Form className="create-new-user-form">
+              <div className="label">
+                <label>
+                  {t("form.first-name")}
 
-            <label>
-              Occupation
-              {/* https://codesandbox.io/s/vxv6q4z5?file=/index.js:943-949 */}
-              <select
-                onChange={handleChange}
-                name="position"
-                value={values.position}
-                required
-              >
-                <option value="" label="Select occupation" disabled hidden />
-                <option value={Positions.SoftwareEngineer}>
-                  {Positions.SoftwareEngineer}
-                </option>
-                <option value={Positions.Painter}>{Positions.Painter}</option>
-                <option value={Positions.BusDriver}>
-                  {Positions.BusDriver}
-                </option>
-              </select>
-            </label>
-            <br />
+                  <Field
+                    className={`input ${
+                      errors.firstName &&
+                      touched.firstName &&
+                      "validation-error"
+                    }`}
+                    name="firstName"
+                    placeholder={t("form.placeholder.first-name")}
+                    required
+                  />
+                  <div className="validation-error-message">
+                    {errors.firstName &&
+                      touched.firstName &&
+                      `${errors.firstName}`}
+                  </div>
+                </label>
+              </div>
 
-            <label>
-              Date of Birth:
-              <DatePicker
-                dayPlaceholder="dd"
-                monthPlaceholder="mm"
-                yearPlaceholder="yyyy"
-                className={`date-picker ${
-                  errors.dateOfBirth && touched.dateOfBirth ? "red" : "green"
-                }`}
-                name="dateOfBirth"
-                value={values.dateOfBirth}
-                // https://stackoverflow.com/questions/56312372/react-datepicker-with-a-formik-form
-                onChange={async (inputValue) => {
-                  console.log(inputValue);
-                  await setFieldValue("dateOfBirth", inputValue, false);
-                  setFieldTouched("dateOfBirth", true);
-                }}
-              />
-              {touched.dateOfBirth && errors.dateOfBirth && (
-                <div>{errors.dateOfBirth}</div>
-              )}
-            </label>
+              <div className="label">
+                <label>
+                  {t("form.last-name")}
+                  <Field
+                    className={`input ${
+                      errors.lastName && touched.lastName && "validation-error"
+                    }`}
+                    name="lastName"
+                    placeholder={t("form.placeholder.last-name")}
+                    required
+                  />
+                  <div className="validation-error-message">
+                    {errors.lastName &&
+                      touched.lastName &&
+                      `${errors.lastName}`}
+                  </div>
+                </label>
+              </div>
 
-            <br />
-            <button type="submit">Submit</button>
-          </Form>
-        )}
-      </Formik>
-      {savedData && <div>Response result: {JSON.stringify(savedData)}</div>}
+              <div className="label">
+                <label>
+                  {t("form.occupation")}
+
+                  <select
+                    className="input"
+                    onChange={handleChange}
+                    name="position"
+                    value={values.position}
+                    required
+                  >
+                    <option
+                      value=""
+                      label={t("form.placeholder.occupation")}
+                      disabled
+                      hidden
+                    />
+                    <option value={Positions.SoftwareEngineer}>
+                      {t("Positions.SoftwareEngineer")}
+                    </option>
+                    <option value={Positions.Painter}>
+                      {t("Positions.Painter")}
+                    </option>
+                    <option value={Positions.BusDriver}>
+                      {t("Positions.BusDriver")}
+                    </option>
+                  </select>
+                </label>
+              </div>
+
+              <div className="label">
+                <label>
+                  {t("form.date-of-birth")}
+                  <DatePicker
+                    dayPlaceholder="dd"
+                    monthPlaceholder="mm"
+                    yearPlaceholder="yyyy"
+                    className={`input ${
+                      errors.lastName && touched.lastName && "validation-error"
+                    }`}
+                    name="dateOfBirth"
+                    value={values.dateOfBirth}
+                    onChange={async (inputValue) => {
+                      console.log(inputValue);
+                      await setFieldValue("dateOfBirth", inputValue, false);
+                      setFieldTouched("dateOfBirth", true);
+                    }}
+                  />
+                  <div className="validation-error-message">
+                    {errors.dateOfBirth &&
+                      touched.dateOfBirth &&
+                      `${errors.dateOfBirth}`}
+                  </div>
+                </label>
+              </div>
+
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+      {savedData && (
+        <div>
+          {t("form.response-result")} {JSON.stringify(savedData)}
+        </div>
+      )}
     </div>
   );
 };
--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,5 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 import EnglishTranslation from "./en/english-translation.json";
 import SpanishTranslation from "./es/spanish-translation.json";
@@ -9,10 +10,16 @@ export const resources = {
   es: { translation: SpanishTranslation },
 };
 
-i18n.use(initReactI18next).init({
-  // debug: true,
-  lng: "en",
-  resources,
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    // debug: true,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources,
+  });
 
 export default i18n;
